480a1a2639c8792cae79afc917c71313467dd544
tanker <tanker@tankerdeiMac.local>
2017-10-25 11:47:21 +0800

    request错误处理修改

--- blog/src/utils/request.js
+++ blog/src/utils/request.js
@@ -1,22 +1,32 @@
 import { Loading, Message } from 'element-ui'
 import axios from 'axios';
-axios.defaults.baseURL = 'http://192.168.1.104:7001';
-axios.defaults.headers = {
-  'Content-Type': 'application/json;charset=utf-8'
-}
-axios.defaults.timeout=10000
-axios.interceptors.request.use(function (config) {
+var instance = axios.create({
+    baseURL: 'http://192.168.1.104:7001',
+    // baseURL: 'http://120.77.203.62:8855/tunnel/check/web',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    },
+    timeout: 10000
+})
+// axios.defaults.baseURL = 'http://192.168.1.104:7001';
+// axios.defaults.headers = {
+//   'Content-Type': 'application/json;charset=utf-8'
+// }
+// axios.defaults.timeout=10000
+instance.interceptors.request.use(function (config) {
   // Do something before request is sent
   // Message.success('开始请求')
    Loading.service()
   return config;
 }, function (error) {
+  console.log(123)
   // Do something with request error
   let loadingInstance1 = Loading.service()
   loadingInstance1.close()
+    console.log(error)
   return Promise.reject(error);
 });
-axios.interceptors.response.use(function (response) {
+instance.interceptors.response.use(function (response) {
   // Do something with response data
   let loadingInstance1 = Loading.service()
   loadingInstance1.close()
@@ -33,7 +43,14 @@ axios.interceptors.response.use(function (response) {
   // Do something with response error
   let loadingInstance1 = Loading.service()
   loadingInstance1.close()
-  Message.error(error)
-  return Promise.reject(error);
+    console.log(error)
+    if(error.code === undefined){
+      Message.error('请求失败')
+    }else if(error.code == "ECONNABORTED"){
+      Message.error('请求超时，请检查网络连接，重新尝试！')
+    }else {
+        Message.error('服务器错误')
+    }
+    return Promise.reject(error);
 });
-export default axios
+export default instance
